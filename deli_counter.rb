0db66253a7143a153a23fe9b8e0bd8dd1a942e0d
Mintra Phophaijit <bluembc@gmail.com>
2019-11-16 00:01:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,25 @@
- def katz_deli = ["current_line_of_people"]
-   number = 1
-   
- puts "Welcome, #{name}. You are number #{current_line_of_people} in line"
- number+=1 
- end
- 
- def now_serving(katz_deli)
-   if katz_deli.length = 0 
-     return "The Line is currently empty"
-   else
-     return ( the line is currently #{current_line_of_people})
-   
-  take_a_number(katz_deli, "Ada")
-  
-  take_a_number(katz_deli, "Grace") 
-  
-  take_a_number(katz_deli, "Kent") 
- 
-  line(katz_deli) 
- 
-  now_serving(katz_deli) 
- 
-  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
- 
-  take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in line.
- 
-  line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
- 
-  now_serving(katz_deli) #=> "Currently serving Grace."
- 
-  line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
-  
-end 
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  elselea
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
