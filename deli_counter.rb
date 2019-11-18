9518a0e38879391d83933b00cad5d65fd5cd7fc3
Oksana Nichols <oksanahnichols@yahoo.com>
2019-11-18 06:51:41 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,6 @@
-# Write your code here.
+# Write your code here
+describe 'Deli Counter' do
+
+  let(:katz_deli) { [] }
+  let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+  let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
